ccobbster/localshop

commit 91241fd959ac784d251c7f308a4837982acc51ef

Implemented credential system.
- Added Credential model with access and secret key, CRUD interface and auth backend able to check them.
- Refactored handling of basic auth request headers.

localshop/apps/packages/forms.py
@@ -27,7 +27,7 @@ class ReleaseFileForm(forms.ModelForm):
 
     def save(self, commit=True):
         obj = super(ReleaseFileForm, self).save(False)
-        obj.python_version = self.cleaned_data['pyversion'] or 'source'
+        obj.python_version = self.cleaned_data['pyversion'] or 'n/a'
         if commit:
             obj.save()
         return obj

localshop/apps/packages/models.py
@@ -12,6 +12,7 @@ from localshop.apps.packages.utils import OverwriteStorage
 
 from docutils.utils import SystemMessage
 
+
 class Classifier(models.Model):
     name = models.CharField(max_length=255, unique=True)
 

localshop/apps/packages/utils.py
@@ -1,5 +1,6 @@
 import inspect
 
+from django.conf import settings
 from django.core.files.storage import FileSystemStorage
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.http import HttpResponseForbidden
@@ -8,8 +9,9 @@ from django.utils.decorators import method_decorator
 from django.views.generic.base import View
 from django.views.decorators.csrf import csrf_exempt
 
-from localshop.conf import settings
 from localshop.apps.permissions.models import CIDR
+from localshop.apps.permissions.utils import (credentials_required,
+                                              credential_check_needed)
 
 
 class OverwriteStorage(FileSystemStorage):
@@ -31,9 +33,9 @@ class OverwriteStorage(FileSystemStorage):
 
 
 def validate_client(func):
-    """Only allow downloads from authenticted users or from remote ip's
-    which are listed in `LOCALSHOP_ALLOWED_REMOTE_IPS`
-
+    """
+    Only allow downloads from authenticted users or from remote ip's
+    that match one of the ones in the CIDR database.
     """
     if inspect.isclass(func) and issubclass(func, View):
         original_dispatch = func.dispatch
@@ -53,6 +55,9 @@ def validate_client(func):
         if CIDR.objects.has_access(request.META['REMOTE_ADDR']):
             return func(request, *args, **kwargs)
         return HttpResponseForbidden('No permission')
+
+    if credential_check_needed:
+        _wrapper = credentials_required(_wrapper)
     return _wrapper
 
 

localshop/apps/packages/views.py
@@ -1,7 +1,5 @@
-import base64
 import logging
 
-from django.conf import settings
 from django.contrib.auth import authenticate
 from django.contrib.auth.decorators import login_required, permission_required
 from django.core.exceptions import ObjectDoesNotExist
@@ -13,16 +11,16 @@ from django.views.decorators.csrf import csrf_exempt
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 
+from localshop.http import HttpResponseUnauthorized
 from localshop.views import LoginRequiredMixin, PermissionRequiredMixin
 from localshop.apps.packages import forms
 from localshop.apps.packages import models
 from localshop.apps.packages import tasks
 from localshop.apps.packages.pypi import get_package_data
 from localshop.apps.packages.utils import parse_distutils_request, validate_client
+from localshop.apps.permissions.utils import split_auth, decode_credentials
 
 logger = logging.getLogger(__name__)
-
-basic_realm = getattr(settings, 'BASIC_AUTH_REALM', 'pypi')
 
 
 @validate_client
@@ -45,19 +43,14 @@ class SimpleIndex(ListView):
         data, files = parse_distutils_request(request)
 
         # XXX: Auth is currently a bit of a hack
-        auth = request.META.get('HTTP_AUTHORIZATION')
-        if not auth:
-            response = HttpResponse('Missing auth header')
-            response.status_code = 401
-            response['WWW-Authenticate'] = 'Basic realm="%s"' % basic_realm
-            return response
-        method, identity = auth.split()
-        username, password = base64.decodestring(identity).split(':')
+        method, identity = split_auth(request)
+        if not method:
+            return HttpResponseUnauthorized('Missing auth header')
+
+        username, password = decode_credentials(identity)
         user = authenticate(username=username, password=password)
         if not user:
-            response = HttpResponse('Invalid username/password')
-            response.status_code = 401
-            return response
+            return HttpResponse('Invalid username/password', status=401)
 
         actions = {
             'submit': handle_register_or_upload,

localshop/apps/permissions/backend.py
@@ -1,0 +1,17 @@
+from django.contrib.auth.backends import ModelBackend
+
+from .models import Credential
+
+
+class CredentialBackend(ModelBackend):
+    def authenticate(self, username=None, password=None):
+        try:
+            credential = Credential.objects.active().get(access_key=username,
+                                                         secret_key=password)
+        except (Credential.DoesNotExist,
+                Credential.MultipleObjectsReturned):
+            pass
+        else:
+            if credential.creator.is_active:
+                return credential.creator
+        return None

localshop/apps/permissions/migrations/0003_auto__add_credential.py
@@ -1,0 +1,82 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Credential'
+        db.create_table('permissions_credential', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('access_key', self.gf('uuidfield.fields.UUIDField')(max_length=32)),
+            ('secret_key', self.gf('uuidfield.fields.UUIDField')(max_length=32)),
+            ('creator', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'])),
+            ('created', self.gf('django.db.models.fields.DateTimeField')(default=datetime.datetime.now)),
+            ('deactivated', self.gf('django.db.models.fields.DateTimeField')(null=True, blank=True)),
+        ))
+        db.send_create_signal('permissions', ['Credential'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Credential'
+        db.delete_table('permissions_credential')
+
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'permissions.cidr': {
+            'Meta': {'object_name': 'CIDR'},
+            'cidr': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '128'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'label': ('django.db.models.fields.CharField', [], {'max_length': '128', 'null': 'True', 'blank': 'True'})
+        },
+        'permissions.credential': {
+            'Meta': {'object_name': 'Credential'},
+            'access_key': ('uuidfield.fields.UUIDField', [], {'max_length': '32'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'creator': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"}),
+            'deactivated': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'secret_key': ('uuidfield.fields.UUIDField', [], {'max_length': '32'})
+        }
+    }
+
+    complete_apps = ['permissions']

localshop/apps/permissions/migrations/0004_auto__add_unique_credential_access_key__add_unique_credential_secret_k.py
@@ -1,0 +1,92 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding index on 'Credential', fields ['access_key']
+        db.create_index('permissions_credential', ['access_key'])
+
+        # Adding unique constraint on 'Credential', fields ['access_key']
+        db.create_unique('permissions_credential', ['access_key'])
+
+        # Adding index on 'Credential', fields ['secret_key']
+        db.create_index('permissions_credential', ['secret_key'])
+
+        # Adding unique constraint on 'Credential', fields ['secret_key']
+        db.create_unique('permissions_credential', ['secret_key'])
+
+
+    def backwards(self, orm):
+        # Removing unique constraint on 'Credential', fields ['secret_key']
+        db.delete_unique('permissions_credential', ['secret_key'])
+
+        # Removing index on 'Credential', fields ['secret_key']
+        db.delete_index('permissions_credential', ['secret_key'])
+
+        # Removing unique constraint on 'Credential', fields ['access_key']
+        db.delete_unique('permissions_credential', ['access_key'])
+
+        # Removing index on 'Credential', fields ['access_key']
+        db.delete_index('permissions_credential', ['access_key'])
+
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'permissions.cidr': {
+            'Meta': {'object_name': 'CIDR'},
+            'cidr': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '128'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'label': ('django.db.models.fields.CharField', [], {'max_length': '128', 'null': 'True', 'blank': 'True'})
+        },
+        'permissions.credential': {
+            'Meta': {'ordering': "['-created']", 'object_name': 'Credential'},
+            'access_key': ('uuidfield.fields.UUIDField', [], {'db_index': 'True', 'unique': 'True', 'max_length': '32', 'blank': 'True'}),
+            'created': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'creator': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"}),
+            'deactivated': ('django.db.models.fields.DateTimeField', [], {'null': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'secret_key': ('uuidfield.fields.UUIDField', [], {'db_index': 'True', 'unique': 'True', 'max_length': '32', 'blank': 'True'})
+        }
+    }
+
+    complete_apps = ['permissions']

localshop/apps/permissions/models.py
@@ -1,6 +1,14 @@
 import netaddr
 from django.db import models
+from django.contrib.auth.models import User
 
+try:
+    from django.utils.timezone import now
+except ImportError:
+    from datetime import datetime
+    now = datetime.now
+
+from uuidfield import UUIDField
 
 
 class CIDRManager(models.Manager):
@@ -23,4 +31,29 @@ class CIDR(models.Model):
     class Meta:
         permissions = (
             ("view_cidr", "Can view CIDR"),
+        )
+
+
+class CredentialManager(models.Manager):
+
+    def active(self):
+        return self.filter(deactivated__isnull=True)
+
+
+class Credential(models.Model):
+    access_key = UUIDField(verbose_name='Access key', help_text='The access key', auto=True, db_index=True)
+    secret_key = UUIDField(verbose_name='Secret key', help_text='The secret key', auto=True, db_index=True)
+    creator = models.ForeignKey(User)
+    created = models.DateTimeField(default=now)
+    deactivated = models.DateTimeField(blank=True, null=True)
+
+    objects = CredentialManager()
+
+    def __unicode__(self):
+        return self.access_key.hex
+
+    class Meta:
+        ordering = ['-created']
+        permissions = (
+            ("view_credential", "Can view credential"),
         )

commit bbaa5657b9243664c218a13cb83bcd3f251c2bfc

Fixed bug in file upload handling introduced in 91241fd959ac784d251c7f308a4837982acc51ef.

localshop/apps/packages/forms.py
@@ -27,7 +27,7 @@ class ReleaseFileForm(forms.ModelForm):
 
     def save(self, commit=True):
         obj = super(ReleaseFileForm, self).save(False)
-        obj.python_version = self.cleaned_data['pyversion'] or 'n/a'
+        obj.python_version = self.cleaned_data['pyversion'] or 'source'
         if commit:
             obj.save()
         return obj